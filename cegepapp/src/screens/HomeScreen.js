e5471b18219a15cbd7e238369e211cfd410a188b
Alexandre Tanguay <2383528@collegemv.qc.ca>
2025-09-19 15:00:34 -0400

    Songs + Starting HomeScreen Cards

--- cegepapp/src/screens/HomeScreen.js
+++ cegepapp/src/screens/HomeScreen.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { getRandomSongs } from '../hooks/RandomSong';
 
 export default function HomeScreen({navigation}) {
+
+    const [featuredSongs, setFeaturedSongs] = useState([]);
+
+    useEffect(() => {
+        const selected = getRandomSongs();
+        setFeaturedSongs(selected);
+    }, []);
+
+    const renderSongItem = ({item}) => {
+        <View>
+            <Image/>
+            <View/>
+        </View>
+    }
+
     return(
         <View style={styles.container}>
             <Text style={styles.title}>Search</Text>
             <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Details')}><Text>See Details</Text></TouchableOpacity>
             <View></View>
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Counter')}><Text>See Counter</Text></TouchableOpacity>
         </View>
     )
     
@@ -36,4 +51,15 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         backgroundColor: 'rebeccapurple',
     },
+    listContainer: {
+        justifyContent: 'space-between'
+    },
+    songCard : {
+        flexDirection: 'row',
+        alignItems: "center",
+        padding: 12,
+        marginRight: 16,
+        marginBottom: 20,
+        
+    }
 });
