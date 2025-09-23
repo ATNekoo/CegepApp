29e2581b3daeb1b5cd2a0c4063e41f8e42eb725d
Marek Gromko <marek.gromko@hotmail.ca>
2025-09-23 10:38:37 -0400

    theme: add theme to SearchScreen

--- cegepapp/src/screens/SearchScreen.js
+++ cegepapp/src/screens/SearchScreen.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
-import { View, StyleSheet, ScrollView, FlatList } from 'react-native';
+import { useState, useMemo, useContext } from "react";
+import { View, StyleSheet, FlatList } from 'react-native';
 import data from "../data/songs.json";
 import SearchBar from "../components/SearchBar";
 import SelectionBar from "../components/SelectionBar";
 import SongCard from "../components/SongCard";
 import SongPlayerModal from "../hooks/MusicPlayer";
+import { useStyleFactory } from "../context/ThemeContext";
 
 const SEARCH_OPTIONS = {
     'genre': 'Genre',
@@ -27,10 +28,10 @@ const SearchScreen = ()=>{
     const [selectedItem, setSelectedItem] = useState(null);
     const [currentOption, setCurrentOption] = useState('song_title');
     const [currentData, setCurrentData] = useState([]);
-
     const handleSearch = (hint)=>{
         setCurrentData(filterSong(currentOption, hint));
     }
+    const styles = useStyleFactory(styleFactory);
 
     return (
         <View style={styles.page}>
@@ -70,5 +71,18 @@ const styles = StyleSheet.create({
     }
 })
 
+const styleFactory = (colors)=>(console.log(colors), {
+    page: {
+        backgroundColor: colors.background,
+        display: 'flex',
+        overflow: 'hidden',
+        height: '100%',
+    },
+    content: {
+        marginTop: 12,
+        padding: 20,
+        paddingTop: 8,
+    }
+});
 
 export default SearchScreen;
