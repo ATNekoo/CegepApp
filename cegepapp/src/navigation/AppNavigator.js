f796c8e568be233107255816c0a4fc8c33ca04c6
Marek Gromko <marek.gromko@hotmail.ca>
2025-09-22 22:55:02 -0400

    fix: with new tab layout

--- cegepapp/src/navigation/AppNavigator.js
+++ cegepapp/src/navigation/AppNavigator.js
@@ -29,6 +29,7 @@ function Tabs() {
       }}
     >
       <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Search" component={SearchScreen} options={{tabBarIcon: makeTabIcon(require("../icons/search-icon.svg"))}}/>
       <Tab.Screen name="Playlist" component={Playlists} />
       <Tab.Screen name="Chatbot" component={ChatbotScreen} />
       <Tab.Screen name="Favorites" component={FavoriteSongsScreen} />
