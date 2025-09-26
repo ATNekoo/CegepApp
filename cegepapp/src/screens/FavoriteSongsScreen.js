011683be0a58003a339454a1176db66e0fd81272
Alexandre Tanguay <2383528@collegemv.qc.ca>
2025-09-26 13:47:14 -0400

    Theme dans favori

--- cegepapp/src/screens/FavoriteSongsScreen.js
+++ cegepapp/src/screens/FavoriteSongsScreen.js
@@ -1,24 +1,26 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from 'react';
 import { View, Text, FlatList, StyleSheet, Image, TouchableOpacity } from "react-native";
 import { useLiked } from "../context/FavoriteContext"; // ⚠️ même chemin partout
+import { ThemeContext } from "../context/ThemeContext";
 
 export default function FavoriteSongsScreen() {
   const { likedSongs } = useLiked();
+  const { colors } = useContext(ThemeContext)
 
   const renderItem = ({ item }) => (
-    <View style={styles.card}>
+    <View style={[styles.card, {backgroundColor: colors.background, borderColor: colors.border}]}>
       {!!item.image && <Image source={{ uri: item.image }} style={styles.image} />}
       <View style={{ flex: 1 }}>
-        <Text style={styles.title}>{item.song_title}</Text>
-         <Text style={styles.artist}>{item.artist_name}</Text>
-       <Text style={styles.album}>{item.album_name}</Text>
+        <Text style={[styles.title, {color: colors.text}]}>{item.song_title}</Text>
+         <Text style={[styles.artist, {color: colors.text}]}>{item.artist_name}</Text>
+       <Text style={[styles.album, {color: colors.textDim}]}>{item.album_name}</Text>
       </View>
     </View>
   );
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.header}>My  favorite Songs :   {likedSongs.length}</Text>
+    <View style={[styles.container, {backgroundColor: colors.background, }]}>
+      <Text style={[styles.header, {color: colors.text}]}>My  favorite Songs :   {likedSongs.length}</Text>
 
       <FlatList
         data={likedSongs}
@@ -41,7 +43,7 @@ export default function FavoriteSongsScreen() {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: "#323643", padding: 20 },
+  container: { flex: 1, padding: 20 },
   header: { color: "#fff", fontSize: 22, fontWeight: "bold", marginBottom: 15 },
   card: {
     flexDirection: "row",
@@ -50,11 +52,11 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     padding: 10,
     marginBottom: 10,
-    borderWidth: 1,
+    borderWidth: 2,
     borderColor: "#93deff",
   },
   image: { width: 60, height: 60, borderRadius: 6, marginRight: 10 },
-  title: { color: "#fff", fontSize: 16, fontWeight: "600" },
-  artist: { color: "#ccc", fontSize: 14 },
-  album: { color: "#aaa", fontSize: 12 },
+  title: { fontSize: 16, fontWeight: "600" },
+  artist: { fontSize: 14 },
+  album: { fontSize: 12 },
 });
